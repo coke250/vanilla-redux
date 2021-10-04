3937c748b24fc5ae7fb2cd413ff4a7932a4155c5
SSABI <fhy1930@gmail.com>
2021-10-05 01:10:25 +0900

    PURE REDUX: TO DO LIST

--- src/index.js
+++ src/index.js
@@ -1,57 +1,73 @@
-import { createStore } from "redux";
+import { createStore } from 'redux';
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
 // 액션 타입 정의
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  }
+}
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id,
+  }
+}
 
 const reducer = (state = [], action) => {
-    console.log(action);
-    switch (action.type) {
-        case ADD_TODO:
-            return [...state, { text: action.text, id: Date.now() }];
-        case DELETE_TODO:
-            return [];
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ADD_TODO:
+      const newToDoObj = { text: action.text, id: Date.now() }
+      return [...state, newToDoObj];
+    case DELETE_TODO:
+      const cleaned = state.filter(toDo => toDo.id !== action.id);
+      return cleaned;
+    default:
+      return state;
+  }
 };
 
 const store = createStore(reducer);
 
-const addToDo = (text) => {
-    store.dispatch({ type: ADD_TODO, text });
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
 };
 
-const deleteToDo = (e) => {
-  console.log(e.target.parentNode);
-}
+const dispatchDeleteToDo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
 
 const paintTodos = () => {
-    const toDos = store.getState();
-    ul.innerHTML = "";
-    toDos.forEach((toDo) => {
-        const li = document.createElement("li");
-        const btn = document.createElement('button');
-        btn.innerText='DEL';
-        btn.addEventListener('click', deleteToDo)
-        li.id = toDo.id;
-        li.innerText = toDo.text;
-        li.appendChild(btn);
-        ul.appendChild(li);
-    });
+  const toDos = store.getState();
+  ul.innerHTML = '';
+  toDos.forEach((toDo) => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'DEL';
+    btn.addEventListener('click', dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
 };
 
 store.subscribe(paintTodos);
 
 const onSubmit = (e) => {
-    e.preventDefault();
-    const toDo = input.value;
-    input.value = "";
-    addToDo(toDo);
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  dispatchAddToDo(toDo);
 };
 
-form.addEventListener("submit", onSubmit);
+form.addEventListener('submit', onSubmit);
